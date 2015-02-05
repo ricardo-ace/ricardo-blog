87827ebdbb7e8b95fb2a625a8a93652a1c151aa2
ricardo acevedo <ricardojracevedo@yahoo.com>
2015-02-05 15:03:58 -0800

    ds

--- controller/create-db.php
+++ controller/create-db.php
@@ -7,18 +7,30 @@ require_once(__DIR__ . "/../model/database.php");
 $connection = new mysqli($host, $username, $password);
 
 //   this is to see if my code that is above works like if their is an error
-if($connection->connect_error) {
+if ($connection->connect_error) {
     die("Error: " . $connection->connect_error);
 }
-  $exists = $connection->select_db($database);
-    
-  if(!$exists){
+$exists = $connection->select_db($database);
+
+if (!$exists) {
     $query = $connection->query("CREATE DATABASE $database");
-    
-    if($query){
-       echo "successfully created database" . $database;
+
+    if ($query) {
+        echo "successfully created database" . $database;
     }
-     }
+} else {
+    echo "database already exists ";
+}
+echo "duh";
+$query = $connection->query("CREATE TABLE posts ("
+        . "id int(11) NOT NULL AUTO_INCREMENT,"
+        . "title varchar(255) NOT NULL,"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query) {
+    echo "succesfully created table: posts";
+}
 
 $connection->close();
 
