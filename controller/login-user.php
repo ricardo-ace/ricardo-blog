33d7f2bbf9bd029afe42010c82c1ec6616f9c719
ricardo acevedo <ricardojracevedo@yahoo.com>
2015-02-23 15:03:42 -0800

    cs

---
+++ controller/login-user.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(__DIR__ . "/../model/config.php");
+
+$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+ query
